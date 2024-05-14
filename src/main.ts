bb04ee92d5b0ac9d3d2ec983805886cfc5058edf
Harold Vanderpool <hbvanderpool@gmail.com>
2024-05-14 15:56:46 -0700

    Provide an ignore for Typescript for router and store

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+// @ts-ignore
 import router from './router'
+// @ts-ignore
 import store from './store'
 
 Amplify.configure(outputs);
