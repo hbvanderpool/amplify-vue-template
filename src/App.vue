0c04d70754c930d33351c2cbc74bee3fc041bd41
Harold Vanderpool <hbvanderpool@gmail.com>
2024-05-14 15:30:55 -0700

    port over the vue example: Real-World_Vue-3

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,38 @@ import Todos from './components/Todos.vue'
 </script>
 
 <template>
-  <main>
-    
-    <authenticator>
-      <template v-slot="{ user, signOut }">
-        <h1>Hello {{ user?.signInDetails?.loginId}}'s todos</h1>
-        <Todos />
-        <button @click="signOut">Sign Out</button>
-      </template>
-    </authenticator>
-
-  </main>
+  <div id="app">
+    <div id="nav">
+      <router-link :to="{ name: 'EventList' }">Events</router-link> |
+      <router-link :to="{ name: 'About' }">About</router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
 
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+h4 {
+  font-size: 20px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import router from './router'
+import store from './store'
 
 Amplify.configure(outputs);
 
-createApp(App).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount('#app');
