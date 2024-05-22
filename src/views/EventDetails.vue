e7d5f8758379f812b4e04584116a3705fbbf7083
Harold Vanderpool <hbvanderpool@gmail.com>
2024-05-22 15:01:08 -0700

    Use json to parse response into object array

--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -1,3 +1,26 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import EventService from '@/services/EventService.js'
+
+const props = defineProps({
+  id: {
+    required: true,
+  },
+})
+
+const event = ref(null)
+
+onMounted(() => {
+  EventService.getEvent(props.id)
+    .then((response) => {
+      event.value = response.data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
+</script>
+
 <template>
   <div v-if="event">
     <h1>{{ event.title }}</h1>
@@ -5,24 +28,3 @@
     <p>{{ event.description }}</p>
   </div>
 </template>
-
-<script>
-import EventService from '@/services/EventService.js'
-export default {
-  props: ['id'],
-  data() {
-    return {
-      event: null
-    }
-  },
-  created() {
-    EventService.getEvent(this.id)
-      .then(response => {
-        this.event = response.data
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
-}
-</script>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,35 +1,29 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
+
+const events = ref(null)
+
+onMounted(() => {
+  EventService.getEvents()
+    .then((response) => {
+      let items = JSON.parse(response.data.body)
+      console.log(items)
+      events.value = items
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
+</script>
+
 <template>
-  <h1>Events for Good</h1>
+  <h1>Events For Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
-
-<script>
-import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService.js'
-
-export default {
-  name: 'EventList',
-  components: {
-    EventCard
-  },
-  data() {
-    return {
-      events: null
-    }
-  },
-  created() {
-    EventService.getEvents()
-      .then(response => {
-        this.events = response.data
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
-}
-</script>
 
 <style scoped>
 .events {
