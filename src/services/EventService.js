639aeaba21819048b97f489d7e3855c82c7967ae
Harold Vanderpool <hbvanderpool@gmail.com>
2024-05-21 17:00:29 -0700

    separate the OPTIONS request to support CORS preflight

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://upzml0i5fk.execute-api.us-east-2.amazonaws.com/dev',
+  baseURL: 'https://2v2w8rviu8.execute-api.us-east-2.amazonaws.com/dev',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
@@ -11,7 +11,7 @@ const apiClient = axios.create({
 
 export default {
   getEvents() {
-    return apiClient.get('/FetchEvents')
+    return apiClient.get('/events')
   },
   getEvent(id) {
     return apiClient.get('/events/' + id)
